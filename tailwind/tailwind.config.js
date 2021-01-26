a1a90766df96cddf8042fa4edd34cdd2fe47d2f3
Minh Le <minh.le@construct.sg>
2021-01-26 16:58:02 +0700

    update files

---
+++ tailwind/tailwind.config.js
@@ -0,0 +1,13 @@
+var { themes, variants, plugins } = require("./tailwind.extend");
+
+module.exports = {
+  purge: false,
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: themes,
+  },
+  variants: {
+    extend: variants,
+  },
+  plugins: plugins,
+};
---
+++ webpack.mix.js
@@ -0,0 +1,39 @@
+let mix = require("laravel-mix");
+require("mix-tailwindcss");
+require("laravel-mix-clean");
+require("laravel-mix-nunjucks");
+require('laravel-mix-copy-watched');
+
+const outputDir = "dist/src";
+
+// clean output first
+mix.clean();
+
+// do tasks
+mix.setPublicPath(outputDir);
+mix.js("src/js/**/*.js", `${outputDir}/js/main.js`);
+mix.sass("src/css/main.scss", `${outputDir}/css`);
+mix
+  .sass("src/css/libs/tailwind.scss", `${outputDir}/css`)
+  .tailwind("./tailwind/tailwind.config.js");
+mix.njk("src/views/*.njk", 'dist', {
+  ext: ".html",
+  // data: {},
+  // marked: null,
+  // envOptions: null,
+  // manageEnv: (nunjucks) => {},
+});
+
+mix.copyDirectory([
+  'src/libs',
+], `${outputDir}/libs`);
+mix.copyDirectory([
+  'src/images',
+], `${outputDir}/images`);
+
+mix.browserSync({
+  server: {
+    baseDir: "./dist",
+    index: "/index.html",
+  },
+});
